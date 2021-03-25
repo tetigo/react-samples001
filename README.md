fda1080eb50554595a54de8ba0f7c78207d35114
tetigo <tetigo@gmail.com>
2021-03-25 20:45:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tiago-mendes-react01
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/tiago-mendes-react01)
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return React.createElement("h1", null, "Texto01b");
+}
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
