33c9bbccc3213c9a31a5247d86bb867b1625fd7c
tetigo <tetigo@gmail.com>
2021-03-25 22:54:36 -0300

    1 file modified

--- src/Square/Square.js
+++ src/Square/Square.js
@@ -2,17 +2,16 @@ import React from "react";
 import "./Square.css";
 
 function Square({ color }) {
+  const styled = {
+    backgroundColor: color,
+    width: "50px",
+    height: "50px"
+  };
   return (
     <div>
       <div className="square" />
       <br />
-      <div
-        style={{
-          backgroundColor: color,
-          width: "50px",
-          height: "50px"
-        }}
-      />
+      <div style={styled} />
     </div>
   );
 }
